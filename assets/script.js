fceb5e6ea785ee10cf7eaead3fcc431eeba462dc
jnnh <jenniferhorvath@cmail.carleton.ca>
2021-12-19 16:39:11 -0500

    functional answer and question generation

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 var buttonEL= document.querySelector("#start");
 var headingEl= document.querySelector(".heading");
 var questionEl= document.querySelector(".question");
-var randomQuestion = document.querySelector("#randomQuestion");
+var randomQuestion = document.getElementById("randomQuestion");
+var randomAnswers = document.querySelector(".choices");
 questionCounter=1;
 var random = function(array){
     var value = Math.floor(Math.random()*array.length);
@@ -9,7 +10,7 @@ var random = function(array){
 }
 
 
-var colours = [".red",".blue", ".yellow",".green",".orange",".purple"];
+var colours = ["red","blue", "yellow","green","orange","purple"];
 var texts = ["RED","BLUE", "YELLOW","GREEN","ORANGE","PURPLE"];
 var question = {
     class: function (){
@@ -18,8 +19,11 @@ var question = {
     textContent: function(){
         return texts[random(texts)]
     }
-}
-var answers = [];
+};
+var answer = {
+    options: [],
+    answer: ""
+};
 
 var startQuiz = function(){
     buttonEL.remove();
@@ -31,18 +35,35 @@ var startQuiz = function(){
 var displayQuestion = function(){
     var questionText= question.textContent();
     var questionColour= question.class();
-    randomQuestion.textContent = questionText;
+    randomQuestion.innerHTML = questionText;
     randomQuestion.className = questionColour;
-    console.log(questionText);
-    console.log(questionColour);
-    console.log(randomQuestion);
+    createAnswers(questionColour);
 }
 
-var createAnswers = function () {
-
-}
-
-
+var createAnswers = function (answer) {
+    var answers = [0,1,2,3];
+    answers.splice(Math.floor(Math.random()*answers.length),1,answer);
+    console.log(answers);
+    for (var i =0; i<answers.length; i++) {
+        if (typeof(answers[i]) === "number"){
+            var generateOption = function(){
+                var possibleOption = colours[random(colours)];
+                return possibleOption;
+            }
+            var option = generateOption();
+            while (answers.includes(option)){
+                var option = generateOption();
+            }
+            answers.splice(i,1,option);
+        }
+    }
+    for (var i =0; i<answers.length; i++){
+        var listItemEl = document.createElement("li");
+        listItemEl.textContent = answers[i];
+        listItemEl.setAttribute("data-question-Id", i);
+        randomAnswers.appendChild(listItemEl);
+    }
+};
 
 
 console.log(question);
